0a781944be822d29fb52f2eb5994cf6df4835f9f
Matt Copperwaite <mattcopp+git@gmail.com>
2014-02-20 20:50:50 +0000

    changed historic events from being an object for user to being a callback that gets passed through

--- app.js
+++ app.js
@@ -15,7 +15,16 @@ module.exports = {
         app.get('/', function index (req, res) {
             var user = res.locals.user;
             if (user) {
-                res.render("statement", { "now": new Date() });
+                user.historic_events(function (err, events) {
+                    console.log(err, events);
+                    if (!err) {
+                        res.render("statement", { "now": new Date(), "events": events});
+                    }
+                    else {
+                        res.locals.flash("danger", "Database Error.", "Very sorry but there appears to be a problem with the database.");
+                        res.redirect("/");
+                    }
+                });
             }
             else {
                 res.locals.flash("warning", "Invalid member.", "Please sign in.");
